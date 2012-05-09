bdfed0fa420073a29272a7f187799293a50b2d8e
Clément Hussenot <Chussenot@gmail.com>
2012-05-09 15:07:41 +0200

    add user role association

--- lib/drupal/role.rb
+++ lib/drupal/role.rb
@@ -5,5 +5,12 @@ module Drupal
     
     property :rid, Serial
     property :name, String
+
+    has n, :user_roles, 
+      :child_key => [:rid],
+      :repository => Drupal::Repository
+
+    has n, :users, :through => :user_roles  
+    
   end 
 end   
--- lib/drupal/user.rb
+++ lib/drupal/user.rb
@@ -21,6 +21,12 @@ module Drupal
     has n, :comments, 
       :child_key => [:uid],
       :repository => Drupal::Repository  
+
+    has n, :user_roles, 
+      :child_key => [:uid],
+      :repository => Drupal::Repository
+      
+    has n, :roles, :through => :user_roles  
     
     def profile
       Drupal::Node.first :type => 'profile', :uid => uid
@@ -28,5 +34,21 @@ module Drupal
 
     #property :created, Date 
     #def created_at ; Time.at(self[:created]) ; end
+  end
+
+  class UserRole
+    eval Drupal.common
+    storage_names[:drupal] = 'users_roles'
+
+    property :rid, Integer
+    property :uid, Integer
+
+    belongs_to :role, 
+      :child_key => [:rid],
+      :repository => Drupal::Repository  
+
+    belongs_to :user, 
+      :child_key => [:uid],
+      :repository => Drupal::Repository  
   end
 end
